adbe548258871092ccc90d249356053ae2c4181f
keranair <carenwang.6@gmail.com>
2022-06-12 13:43:18 +0700

    jumping script
    added jumping script

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -9,23 +9,63 @@ public class CharacterMoveController : MonoBehaviour
     public float maxSpeed;
     private Rigidbody2D rig;
 
+    [Header("Jump")]
+    public float jumpAccel;
+    private bool isJumping;
+    private bool isOnGround;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        // calculate velocity vector
         Vector2 velocityVector = rig.velocity;
-        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
-        0.0f, maxSpeed);
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
         rig.velocity = velocityVector;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+            }
+        }
     }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+
 }
